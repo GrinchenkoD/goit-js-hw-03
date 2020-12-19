a0cf03cc76bc3449a07874eaa6117a0a8c6e9aee
Dmitry Grinchenko <ra1d3r@i.ua>
2020-12-19 23:15:22 +0200

    Выполнены задания 1-6

---
+++ js/task-05.js
@@ -0,0 +1,30 @@
+'use strict';
+// получает массив объектов и имя свойства.
+// Возвращает массив значений определенного свойства prop из каждого объекта в массиве.
+
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  let allPropValues = [];
+  for (const obj of arr) {
+    if (obj[prop] === undefined) {
+      continue;
+    }
+    allPropValues.push(obj[prop]);
+  }
+  return allPropValues;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
